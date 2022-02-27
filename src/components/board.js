aa6422f3b3a3d806cc99fbd1d8db6443be427251
46chris <chris01wy@gmail.com>
2022-02-27 15:33:08 -0800

    Added stuff

--- src/components/board.js
+++ src/components/board.js
@@ -3,35 +3,49 @@ import styled from 'styled-components';
 import Pawn from './pawn'; 
 
 //Testing out Javascript style object
-const myStyle = { 
-    width: '100%', 
-    maxWidth: '90vh',
-    display:'block', 
-    margin: 'auto'
-}
+// const myStyle = { 
+//     width: '100%', 
+//     maxWidth: '90vh',
+//     display:'block', 
+//     margin: 'auto'
+// }
+
+const Div = styled.div`
+width: min(100vw, 100vh); 
+height: min(100vw,100vh); 
+margin:auto; 
+padding: 5vh; 
+box-sizing: border-box; 
+
+background-image:url(${boardSrc}); 
+background-repeat:no-repeat; 
+background-origin: content-box; 
+background-size: min(100%, 90vh); 
+
+display: grid; 
+grid-template-columns: repeat(8, 1fr);
+grid-template-rows: repeat(8, 1fr);  
+`;  
 
 export default function Board(props){
     //Testing out styled components  
-    const Div = styled.div`
-        width: min(100vw, 100vh); 
-        height: min(100vw,100vh); 
-        margin:auto; 
-        padding: 5vh; 
-        box-sizing: border-box; 
  
-        background-image:url(${boardSrc}); 
-        background-repeat:no-repeat; 
-        background-origin: content-box; 
-        background-size: min(100%, 90vh); 
-
-        display: grid; 
-        grid-template-columns: repeat(8, 1fr);
-        grid-template-rows: repeat(8, 1fr);  
-    `;   
 
     return (
         <Div>
-            <Pawn c={1} r={2}></Pawn>
+            {/* White */}
+            {
+                [1,2,3,4,5,6,7,8].map((index)=>{
+                    return <Pawn c={index} r={7}></Pawn>
+                })
+            }
+
+            {/*Black*/}
+            {
+                [1,2,3,4,5,6,7,8].map((index)=>{
+                    return <Pawn c={index} r={2}></Pawn>
+                })
+            }
         </Div>    
     )
 }
--- src/components/pawn.js
+++ src/components/pawn.js
@@ -6,11 +6,13 @@ import pawnSrc from "../assets/wPawn.png";
 export default function Pawn(props){ 
 
     //This sets the position of the piece on the grid 
-    const Div = styled.div`
-        grid-column: ${props.c}; 
-        grid-row: ${props.r}; 
-    `;
+    const gridPlace = { 
+        gridColumn: props.c, 
+        gridRow: props.r, 
+        padding: "0.4vh"
+    }; 
+
     return (
-        <Div><img style={{display:'block', width:'100%', boxSizing:'border-box'}}src={pawnSrc}></img></Div>
+        <div style={gridPlace}><img style={{display:'block', width:'100%', boxSizing:'border-box'}}src={pawnSrc}></img></div>
     )
 }
